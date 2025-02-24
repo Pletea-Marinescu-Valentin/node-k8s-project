e04cf51a375ef7fad1ae2073d6024a00e3ef042a
Pletea-Marinescu Valentin <pletea.valentin2003@gmail.com>
2025-02-24 23:46:05 +0200

    Add /about endpoint with app description

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App running in Kubernetes! 🚀');
+  res.send('Hello from Dockerized Node.js App running in Kubernetes!');
 });
 
 app.listen(PORT, () => {
@@ -13,3 +13,8 @@ app.listen(PORT, () => {
 app.get('/health', (req, res) => {
   res.status(200).send('OK');
 });
+
+app.get('/about', (req, res) => {
+  res.send('This is a Dockerized Node.js app running in Kubernetes!');
+});
+
